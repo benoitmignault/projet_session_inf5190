4596e57d72d081e95feeecf778b598a442f64bb4
Ben <b.mignault@gmail.com>
2020-03-22 13:56:47 -0400

    Optimisation de la fonction get_restaurant_trouver en créant 4 new fcts.

--- modules/database.py
+++ modules/database.py
@@ -33,81 +33,126 @@ class Database:
 
     def get_restaurant_trouver(self, liste_champs):
         cursor = self.get_connection().cursor()
-        select = "select proprietaire, categorie, etablissement, no_civique, " \
-                 "nom_rue, ville, description, date_infraction," \
-                 "date_jugement, montant_amende, id_resto "
-        fromm = "from mauvais_restaurants "
-        where = "where "
-        nb_critere = 1
-        # Création d'un dictionnaire de critère exclusif
-        liste_critere = {"proprietaire": liste_champs['proprietaire'],
-                         "etablissement": liste_champs['etablissement'],
-                         "nom_rue": liste_champs['nom_rue']}
 
-        # Cette section pour la création du where dynamiquement
-        for cle, valeur in liste_critere.items():
-            if len(valeur) != 0:
-                if cle == "proprietaire":
-                    where += "proprietaire like ? "
+        liste_critere = remplissage_condition_sql(liste_champs)
+        sql = creation_requete_sql(liste_critere, liste_champs['nb_critere'])
+        result = execution_requete_dynamique(liste_champs['nb_critere'],
+                                             liste_critere, sql, cursor)
 
-                if cle == "etablissement":
-                    where += "etablissement like ? "
-
-                if cle == "nom_rue":
-                    where += "nom_rue like ? "
-
-                # Ici, nous savons qu'il reste encore des critères
-                if nb_critere < liste_champs['nb_critere']:
-                    where += "and "
-                    nb_critere += 1
-
-        sql = select + fromm + where
-        proprietaire = "%" + liste_critere['proprietaire'] + "%"
-        proprietaire = proprietaire.upper()
-        etablissement = "%" + liste_critere['etablissement'] + "%"
-        etablissement = etablissement.upper()
-        nom_rue = "%" + liste_critere['nom_rue'] + "%"
-
-        if liste_champs['nb_critere'] == 1:
-            if len(liste_critere["proprietaire"]) != 0:
-                cursor.execute(sql, (proprietaire,))
-
-            elif len(liste_critere["etablissement"]) != 0:
-                cursor.execute(sql, (etablissement,))
-
-            else:
-                cursor.execute(sql, (nom_rue,))
-
-        elif liste_champs['nb_critere'] == 2:
-            if len(liste_critere["proprietaire"]) != 0 and len(
-                    liste_critere["etablissement"]) != 0:
-                cursor.execute(sql, (proprietaire, etablissement))
-
-            elif len(liste_critere["proprietaire"]) != 0 and len(
-                    liste_critere["nom_rue"]) != 0:
-                cursor.execute(sql, (proprietaire, nom_rue))
-
-            else:
-                cursor.execute(sql, (etablissement, nom_rue))
-
-        else:
-            cursor.execute(sql, (proprietaire, etablissement, nom_rue))
-
-        result = cursor.fetchall()
-        ensemble_trouve = {}
-        if result is not None:
-            for un_resto_trouve in result:
-                sous_ensemble = {'Propriétaire': un_resto_trouve[0],
-                                 'Catégorie': un_resto_trouve[1],
-                                 'Établissement': un_resto_trouve[2],
-                                 'Adresse':
-                                     un_resto_trouve[3] + " " +
-                                     un_resto_trouve[4],
-                                 'Ville': un_resto_trouve[5],
-                                 'Description': un_resto_trouve[6],
-                                 "Date d'infraction": un_resto_trouve[7],
-                                 'Date de jugement': un_resto_trouve[8],
-                                 "Montant de l'amende": un_resto_trouve[9]}
-                ensemble_trouve[un_resto_trouve[10]] = sous_ensemble
+        ensemble_trouve = recuperation_resultat_requete(result)
 
         return ensemble_trouve
+
+
+def remplissage_condition_sql(liste_champs):
+    # La préparation des critères en vue d'utiliser l'opérateur like aura
+    # maintenant une longueur minimale de 2 charactères pour un critère
+    # qui aurait été non utiliser par l'Utilisateur
+    proprietaire = "%" + liste_champs['proprietaire'] + "%"
+    proprietaire = proprietaire.upper()
+    etablissement = "%" + liste_champs['etablissement'] + "%"
+    etablissement = etablissement.upper()
+    nom_rue = "%" + liste_champs['nom_rue'] + "%"
+
+    # Création d'un dictionnaire de critère exclusif
+    liste_critere = {"proprietaire": proprietaire,
+                     "etablissement": etablissement,
+                     "nom_rue": nom_rue}
+
+    return liste_critere
+
+
+# Le paramètre nb_critere est le nombre de champ qui ne sont pas vide
+# lors de la recherche par l'usager. Le paramètre nb_critere_enconstruction est
+# une condition à savoir si on doit rajouter ou pas un «AND»
+# à la fin de la construction de la section du «WHERE»
+# Pour prendre en compte un critere, sa longueur de doit être supérieur
+# à 2 comme je prépare dans la fonction remplissage_condition_sql
+# les critères de recherches.
+def creation_requete_sql(liste_critere, nb_critere):
+    select = "select proprietaire, categorie, etablissement, no_civique, " \
+             "nom_rue, ville, description, date_infraction," \
+             "date_jugement, montant_amende, id_resto "
+    fromm = "from mauvais_restaurants "
+    where = "where "
+    nb_critere_enconstruction = 1
+    # Cette section pour la création du where dynamiquement
+    for cle, valeur in liste_critere.items():
+        if len(valeur) > 2:
+            if cle == "proprietaire":
+                where += "proprietaire like ? "
+
+            if cle == "etablissement":
+                where += "etablissement like ? "
+
+            if cle == "nom_rue":
+                where += "nom_rue like ? "
+
+            # Ici, nous savons qu'il reste encore des critères
+            if nb_critere_enconstruction < nb_critere:
+                where += "and "
+                nb_critere_enconstruction += 1
+
+    sql = select + fromm + where
+
+    return sql
+
+
+# Comme la recherche peut se faire entre 1 à 3 critères donc l'execution
+# de la requête peut être dynamique en fonction de l'usager
+# Pour prendre en compte un critere, sa longueur de doit être supérieur
+# à 2 comme je prépare dans la fonction remplissage_condition_sql
+# les critères de recherches.
+def execution_requete_dynamique(nb_critere, liste_critere, sql, cursor):
+    if nb_critere == 1:
+        if len(liste_critere["proprietaire"]) > 2:
+            cursor.execute(sql, (liste_critere["proprietaire"],))
+
+        elif len(liste_critere["etablissement"]) > 2:
+            cursor.execute(sql, (liste_critere["etablissement"],))
+
+        else:
+            cursor.execute(sql, (liste_critere["nom_rue"],))
+
+    elif nb_critere == 2:
+        if len(liste_critere["proprietaire"]) > 2 and len(
+                liste_critere["etablissement"]) > 2:
+            cursor.execute(sql, (liste_critere["proprietaire"],
+                                 liste_critere["etablissement"]))
+
+        elif len(liste_critere["proprietaire"]) > 2 and len(
+                liste_critere["nom_rue"]) > 2:
+            cursor.execute(sql, (liste_critere["proprietaire"],
+                                 liste_critere["nom_rue"]))
+
+        else:
+            cursor.execute(sql, (liste_critere["etablissement"],
+                                 liste_critere["nom_rue"]))
+    else:
+        cursor.execute(sql, (liste_critere["proprietaire"],
+                             liste_critere["etablissement"],
+                             liste_critere["nom_rue"]))
+
+    result = cursor.fetchall()
+
+    return result
+
+
+def recuperation_resultat_requete(result):
+    ensemble_trouve = {}
+    if result is not None:
+        for un_resto_trouve in result:
+            sous_ensemble = {'Propriétaire': un_resto_trouve[0],
+                             'Catégorie': un_resto_trouve[1],
+                             'Établissement': un_resto_trouve[2],
+                             'Adresse':
+                                 un_resto_trouve[3] + " " +
+                                 un_resto_trouve[4],
+                             'Ville': un_resto_trouve[5],
+                             'Description': un_resto_trouve[6],
+                             "Date d'infraction": un_resto_trouve[7],
+                             'Date de jugement': un_resto_trouve[8],
+                             "Montant de l'amende": un_resto_trouve[9]}
+            ensemble_trouve[un_resto_trouve[10]] = sous_ensemble
+
+    return ensemble_trouve
