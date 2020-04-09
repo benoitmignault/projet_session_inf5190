9f90d35994d8ed129b81b18a8e6c635931016aa2
Ben <b.mignault@gmail.com>
2020-04-08 23:19:25 -0400

    Ajout de validateur dans le json_schema

--- validateur_json_schema.py
+++ validateur_json_schema.py
@@ -4,28 +4,45 @@ nouvelle_plainte_etablissement = {
     'type': 'object',
     'properties': {
         'etablissement': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 3,
+            "maxLength": 65
         },
         'no_civique': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 1,
+            "maxLength": 5
         },
         'nom_rue': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 3,
+            "maxLength": 35
         },
         'ville': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 3,
+            "maxLength": 40
         },
         'date_visite': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 10,
+            "maxLength": 10,
+            "pattern": '^([0-9]{4})-(1[0-2]|0[1-9])-(3[01]|0[1-9]|[12][0-9])$'
         },
         'prenom_plaignant': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 3,
+            "maxLength": 35
         },
         'nom_plaignant': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 3,
+            "maxLength": 35
         },
         'description': {
-            'type': 'string'
+            'type': 'string',
+            "minLength": 5,
+            "maxLength": 800
         }
     },
     'required': ['etablissement', 'no_civique', 'nom_rue', 'ville',
